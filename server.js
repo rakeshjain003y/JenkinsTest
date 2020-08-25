850b3364fff62eb28a17e4c170bde425c1f8dae0
rakeshjain003y <rakesh.jain@yactraq.net>
2020-08-25 03:47:58 -0700

    change const to var 1

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ app.post('/hooks/github/', githubMiddleware, function(req, res) {
 });
 
 app.listen(port, () => {
-   console.log("Testing 11")
   console.log("Example app listening at http://localhost:${port}")
 })
  
